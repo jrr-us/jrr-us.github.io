4145d514c7876c604a813b3e20bdd3d5f6d8020e
jrr-us <63546492+jrr-us@users.noreply.github.com>
2025-11-10 18:14:35 +0000

    Deploying to gh-pages from @ jrr-us/jrr-us.github.io@a5361c8c20421951910de88c6eea528fc5b4a864 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
+  },{id: "nav-news",
+          title: "news",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/news/";
+          },
+        },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
