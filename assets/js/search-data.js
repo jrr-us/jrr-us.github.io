8953926bc57c0c8f2df0ef73f96eb78a88fb2463
jrr-us <63546492+jrr-us@users.noreply.github.com>
2026-02-18 21:54:30 +0000

    Deploying to gh-pages from @ jrr-us/jrr-us.github.io@47727bc0fe9ee15330107acd89df10acfa5064e7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-people",
+  },{id: "nav-research",
+          title: "research",
+          description: "Check out our main research topics",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/";
+          },
+        },{id: "nav-people",
           title: "people",
           description: "",
           section: "Navigation",
@@ -22,13 +29,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
-          },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
           },
         },{id: "books-the-godfather",
           title: 'The Godfather',
@@ -46,66 +46,26 @@ ninja.data = [{
           section: "News",},{id: "news-prueba-de-anuncio",
           title: 'Prueba de anuncio.',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-beyond-li-batteries",
+          title: 'Beyond Li-batteries',
+          description: "Electro-active materials and their interfaces",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/1_electro-active-materials/";
             },},{id: "projects-architected-carbon-lattices",
           title: 'Architected carbon lattices',
-          description: "for Beyond-Li batteries and their interphases",
+          description: "via 3D printing and pyrolysis",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/carbon-microlattices/";
+              window.location.href = "/projects/2_carbon-microlattices/";
+            },},{id: "projects-2d-nanomaterials",
+          title: '2D nanomaterials',
+          description: "for green energy storage and beyond",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_2d-nanomaterials/";
             },},{id: "projects-catalytic-graphitization",
           title: 'Catalytic graphitization',
           description: "to produce more sustainable graphite",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/catalytic-graphitization/";
-            },},{id: "projects-electro-active-materials",
-          title: 'Electro-active materials',
-          description: "for Beyond-Li batteries and their interphases",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/electro-active-materials/";
+              window.location.href = "/projects/4_catalytic-graphitization/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
