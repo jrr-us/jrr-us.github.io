66e6a26024df7fca52781b0f3a681c8424071d0d
jrr-us <63546492+jrr-us@users.noreply.github.com>
2026-02-26 23:01:25 +0000

    Deploying to gh-pages from @ jrr-us/jrr-us.github.io@eda93e67da839a37989c80617dfd3aae62c642db 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,26 +46,31 @@ ninja.data = [{
           section: "News",},{id: "news-prueba-de-anuncio",
           title: 'Prueba de anuncio.',
           description: "",
-          section: "News",},{id: "projects-beyond-li-batteries",
-          title: 'Beyond Li-batteries',
-          description: "Electro-active materials and their interfaces",
+          section: "News",},{id: "projects-2d-nanomaterials",
+          title: '2D nanomaterials',
+          description: "for green energy storage and beyond",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_electro-active-materials/";
+              window.location.href = "/projects/2d-nanomaterials/";
             },},{id: "projects-architected-carbon-lattices",
           title: 'Architected carbon lattices',
           description: "via 3D printing and pyrolysis",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_carbon-microlattices/";
-            },},{id: "projects-2d-nanomaterials",
-          title: '2D nanomaterials',
-          description: "for green energy storage and beyond",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_2d-nanomaterials/";
+              window.location.href = "/projects/carbon-microlattices/";
             },},{id: "projects-catalytic-graphitization",
           title: 'Catalytic graphitization',
           description: "to produce more sustainable graphite",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_catalytic-graphitization/";
+              window.location.href = "/projects/catalytic-graphitization/";
+            },},{id: "projects-beyond-li-batteries",
+          title: 'Beyond Li-batteries',
+          description: "Electro-active materials and their interfaces",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/electro-active-materials/";
+            },},{id: "projects-radioactive-waste-management",
+          title: 'Radioactive Waste Management',
+          description: "through advanced materials",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/nuclear-waste/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
